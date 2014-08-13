f3f10d24ba530c2b9a43e81a18cf1288898cd414
Barry Hoggard <barry@tristanmedia.com>
2014-08-13 00:28:45 -0400

    progress on views and templates for artworks

--- benefit/urls.py
+++ benefit/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import patterns, url
-from django.views.generic import TemplateView
+from django.views.generic import DetailView, ListView, TemplateView
+from .models import Artwork
 
 urlpatterns = patterns('',
     url(r'^$', 'benefit.views.index', name='index'),
+    url(r'^artworks$', ListView.as_view(
+        model=Artwork, paginate_by=24),
+        name='artworks-list'),
+    url(r'^artworks/(?P<pk>\d+)$', DetailView.as_view(model=Artwork), name='artwork-detail'),
 )
