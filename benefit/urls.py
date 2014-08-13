bf0986f2b404b97d30b047a55b4914a8c71975a7
Barry Hoggard <barry@tristanmedia.com>
2014-08-13 00:34:00 -0400

    set sort on list view

--- benefit/urls.py
+++ benefit/urls.py
@@ -5,7 +5,8 @@ from .models import Artwork
 urlpatterns = patterns('',
     url(r'^$', 'benefit.views.index', name='index'),
     url(r'^artworks$', ListView.as_view(
-        model=Artwork, paginate_by=24),
+        model=Artwork, paginate_by=24,
+        queryset=Artwork.objects.order_by('artist_last_name', 'artist_first_name', 'title')),
         name='artworks-list'),
     url(r'^artworks/(?P<pk>\d+)$', DetailView.as_view(model=Artwork), name='artwork-detail'),
 )
